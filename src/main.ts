c12b9eec6d63b7da4a5d407361605c0c729119b2
Timas Jackus <timas@mediapark.com>
2019-10-20 17:05:42 +0300

    remove dist folder

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import Conv2D from './conv2D';
 import MaxPool from './maxPool';
 import SoftMax from './softMax';
 import { trainingData } from './data';
-import { log } from 'mathjs';
+import { log, divide,subtract } from 'mathjs';
 
-const { images, labels } = trainingData(1000);
+const { images, labels } = trainingData(1);
 
 const forward = (image: number[][], label: number): any => {
     const conv = new Conv2D(8);
     const maxPool = new MaxPool();
     const softMax = new SoftMax(13 * 13 * 8, 10);
+    image = subtract(divide(image, 255), 0.5) as number[][];
     let output = conv.forward(image);
     output = maxPool.forward(output);
     const outputs = softMax.forward(output);
@@ -28,7 +29,7 @@ images.map((image, index) => {
     totalLoss += loss;
     correctDigits += acc;
 
-    const printInterval = 1000;
+    const printInterval = 5;
     if (index % printInterval === printInterval - 1) {
         console.log(`[Step ${index + 1}] Past ${printInterval} steps: Average Loss ${totalLoss / printInterval} | Accuracy: ${correctDigits}`);
         totalLoss = 0;
